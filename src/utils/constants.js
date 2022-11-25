b48afbaf7fd0b82e29ce11dca737e1546aaa709c
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-25 18:44:30 +0530

    feat -added auth constants

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+//User Auth
+
+const LOGIN_USER = "LOGIN";
+const SIGNUP_USER = "SIGNUP";
+const LOAD_USER = "LOAD_USER";
+const LOGOUT_USER = "LOGOUT";
+
+
+
+export {
+    LOGIN_USER,
+    SIGNUP_USER,
+    LOAD_USER,
+    LOGOUT_USER
+}
