a7abc85db21a4848fa140e31b70269e116f9ef16
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-25 18:41:43 +0530

    auth provider

---
+++ src/Context/Provider/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import React,{useContext, useReducer} from 'react';
+import { authReducer } from '../../Reducer/AuthReducer';
+export const AuthContex = React.createContext();
+function AuthProvider({children}) {
+  
+   const [state,dispatch] = useReducer(authReducer,{
+    token:localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null,
+    user:{},
+    isAuthenticated:token ? true : false,
+   }) 
+
+  return (
+   <AuthContex.Provider value={{authState:state, authDispatch:dispatch}}>
+       {children}
+   </AuthContex.Provider>
+  )
+}
+
+const useAuth = ()=>useContext(AuthContex)
+
+export default AuthProvider
