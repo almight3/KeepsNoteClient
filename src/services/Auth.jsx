fda2586d7992ebec535a75e1261a05badd68b811
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-29 11:44:11 +0530

    fix

--- src/services/Auth.jsx
+++ src/services/Auth.jsx
@@ -1,21 +1,18 @@
-import axios from axios;
+import axios from "axios";
 import {
     LOGIN_USER,
     SIGNUP_USER,
     LOAD_USER,
 } from "../utils/constants.js";
 import toast from 'react-hot-toast';
-import {useNavigate} from "react-router-dom";
-
 import KEEPNOTESAPI from "./Api.js"
 
 // register user
-export const signupUser = async(data,dispatch)=>{
+export const signupUser = async(formData,dispatch,navigate)=>{
   
     try{
-        const navigate = useNavigate();
         const res = await axios.post(`${KEEPNOTESAPI}/api/v1/register`,{
-            data
+            formData
         },
         {
             headers: {
@@ -30,29 +27,29 @@ export const signupUser = async(data,dispatch)=>{
         navigate("/notes")
     }
     catch(error){
+        console.log(error)
        toast.error(error.response.data.message)
     }
 };
 
 // login user
-export const loginUser = async(data,dispatch)=>{
-  
+export const loginUser = async(formData,dispatch)=>{
     try{
-        const navigate = useNavigate();
         const res = await axios.post(`${KEEPNOTESAPI}/api/v1/login`,{
-            data
+            formData
         },
         {
             headers: {
                 "Content-Type": "application/json",
             },
         }); 
+
+        localStorage.setItem("token",JSON.stringify(res.data.token)) 
         dispatch({
             type:LOGIN_USER,
             payload:res.data
-        });    
-        toast.success("user loggedin successfully")
-        navigate("/notes")
+        });   
+        toast.success("user loggedin successfully")       
     }
     catch(error){
        toast.error(error.response.data.message)
