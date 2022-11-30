913578ef231bcd18d3471ac4c24185d4be48443f
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-30 21:45:05 +0530

    added more routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import SideBar from './component/SideBar/SideBar'
-import Navbar from './component/Navbar/Navbar'
+import React from 'react';
+import SideBar from './component/SideBar/SideBar';
+import Navbar from './component/Navbar/Navbar';
+import { BrowserRouter,Routes,Route,Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Login from './pages/Auth/Login';
+import Notes from './pages/Notes/Notes';
+import NoteEditor from './pages/Notes/NoteEditor';
+import ProtectedRoutes from './component/ProtectedRoutes/ProtectedRoutes';
+import ReadNote from './pages/Notes/ReadNote';
+import Trash from './pages/Trash/Trash';
+import Search from './pages/Search/Search';
 function App() {
   return (
-   <>
+  <BrowserRouter>
+     <Toaster
+      position="bottom-center"
+      reverseOrder={false}
+      toastOptions={{
+      // Define default options
+         duration: 2000,
+     }}
+    />
      <SideBar />
-    <Navbar />
-   </>
+     <Navbar />
+     <Routes>
+        <Route element={<ProtectedRoutes />}>
+               <Route path="/notes"  element={<Notes />} />
+               <Route path="/"  element={<Navigate to="/notes" />} />
+               <Route path="/notes/new"  element={<NoteEditor />} />
+               <Route path="/note/edit/:id"  element={<NoteEditor />} />
+               <Route path="/note/read/:id" element={<ReadNote />} />
+               <Route path="/search" element={<Search />} />
+               <Route path="/trash"  element={<Trash />} />
+         </Route>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" />
+     </Routes>
+  </BrowserRouter>
   )
 }
 
