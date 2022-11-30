0f90a2fc975e95b6446bcd8c65ddf72f1759f129
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-30 21:46:02 +0530

    added provder for auth and userData

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
+import { AuthProvider } from './Context/Provider/AuthProvider';
+import { UserDataProvider } from './Context/Provider/UserDataProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <AuthProvider>
+     <UserDataProvider>
+     <App />
+     </UserDataProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
