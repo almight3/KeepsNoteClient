5d6c356eca8857ff2df4accdeab03a658181e31c
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-11-30 20:42:54 +0530

    style - show login user name

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {FaUser} from "react-icons/fa"
-
+import { useAuth } from '../../Context/Provider/AuthProvider'
 function Navbar() {
-  return (
+   const {authState:{user}} = useAuth();
+   return (
    <nav className='bg-secondary text-midnight h-[4.5rem] w-[85%] ml-[15%]'>
        <ul className='flex items-center h-full	'>
           <li className='text-2xl grow'>
@@ -11,7 +12,7 @@ function Navbar() {
           <li className='flex mr-8'>
            
              <FaUser size={25} color="#1c1917" className='mx-3' />
-             <span className='text-lg font-medium'>Priyanshu</span>
+             <span className='text-lg font-medium'>{user.name}</span>
           </li>
 
        </ul>
